cecb1cb16389c77bce1932ef8d46afcc2ae1d67b
Keisuke Ota <k.ota109@outlook.com>
2023-11-08 01:37:37 +0900

    create project

---
+++ src/main/java/com/ot/steps/StepsApiApplication.java
@@ -0,0 +1,13 @@
+package com.ot.steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ot/steps/StepsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ot.steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
